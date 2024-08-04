a755ff734618f8a81fd8515b8edb4fdfc09ce341
Francisco <Francisco@192.168.1.10>
2024-08-04 10:07:10 -0300

    Primer Commit, conexion inicial con el repo

---
+++ equipos/src/main/java/com/api/equipos/EquiposApplication.java
@@ -0,0 +1,13 @@
+package com.api.equipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiposApplication.class, args);
+	}
+
+}
